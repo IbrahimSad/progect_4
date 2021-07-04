d268059f89e141945875693ddd44fe35c7198b7d
IbrahimSad <77147115+IbrahimSad@users.noreply.github.com>
2021-07-03 19:53:04 -0700

    project 4 is 2020/7/4

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+/* globel $ ,alert ,console*/
+$(function () {
+
+    'use strict';
+
+    //trigger Header heught
+
+    $('.my-header').height($(window).height());
+
+
+
+    // Simple mode, it styles document scrollbar:
+
+    $("body").niceScroll({
+
+        cursorcolor: "#f7600e",
+
+        cursorborder: "1px solid #f7600e"
+    });
+});
